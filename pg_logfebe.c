880da2e7a416752cd77c6772f454c8b927082c44
Daniel Farina <daniel@heroku.com>
2012-12-17 17:33:21 -0800

    Improve clarity of quasi-byte-string handling
    Per code review by Keith Rarick.
    
    Signed-off-by: Daniel Farina <daniel@heroku.com>

--- pg_logfebe.c
+++ pg_logfebe.c
@@ -696,10 +696,10 @@ logfebe_emit_log_hook(ErrorData *edata)
 	 * Make room for message type byte and length header.  The length header
 	 * must be overwritten to the correct value at the end.
 	 *
-	 * NB: that C string literals have a trailing NUL byte at the end, so leave
-	 * one such NUL byte out of the literal itself.
+	 * NB: that C string literals have a trailing NUL byte at the end, which is
+	 * why sizeof is adjusted.
 	 */
-	appendBinaryStringInfo(&buf, "L\0\0\0", sizeof "L\0\0\0");
+	appendBinaryStringInfo(&buf, "L\0\0\0\0", sizeof "L\0\0\0\0" - 1);
 
 	/*
 	 * Format the output, and figure out how long it is, and frame it
